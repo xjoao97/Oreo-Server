a672f1ed3e3a7ab79c965b72ed0b1826c58251d3
Antônio <antonio.carneiro@etec.sp.gov.br>
2017-10-05 13:51:37 -0300

    Wired
    ...

---
+++ HabboHotel/Items/Wired/Boxes/Effects/GiveUserHanditemBox.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+using Quasar.Communication.Packets.Incoming;
+using Quasar.HabboHotel.Rooms;
+using Quasar.HabboHotel.Users;
+using Quasar.Communication.Packets.Outgoing.Rooms.Chat;
+using Quasar.Communication.Packets.Outgoing.Rooms.Notifications;
+
+namespace Quasar.HabboHotel.Items.Wired.Boxes.Effects
+{
+    class GiveUserHanditemBox : IWiredItem
+    {
+        public Room Instance { get; set; }
+
+        public Item Item { get; set; }
+
+        public WiredBoxType Type { get { return WiredBoxType.EffectGiveUserHanditem; } }
+
+        public ConcurrentDictionary<int, Item> SetItems { get; set; }
+
+        public string StringData { get; set; }
+
+        public bool BoolData { get; set; }
+
+        public string ItemsData { get; set; }
+
+        public GiveUserHanditemBox(Room Instance, Item Item)
+        {
+            this.Instance = Instance;
+            this.Item = Item;
+            this.SetItems = new ConcurrentDictionary<int, Item>();
+        }
+
+        public void HandleSave(ClientPacket Packet)
+        {
+            int Unknown = Packet.PopInt();
+            string Badge = Packet.PopString();
+
+            this.StringData = Badge;
+        }
+
+        public bool Execute(params object[] Params)
+        {
+            if (Params == null || Params.Length == 0)
+                return false;
+
+            Habbo Player = (Habbo)Params[0];
+            if (Player == null || Player.GetClient() == null)
+                return false;
+
+            RoomUser User = Player.CurrentRoom.GetRoomUserManager().GetRoomUserByHabbo(Player.Username);
+            if (User == null)
+                return false;
+
+            if (String.IsNullOrEmpty(StringData))
+                return false;
+
+            var handitem = int.Parse(StringData);
+            User.CarryItem(handitem);
+            User.GetClient().SendMessage(RoomNotificationComposer.SendBubble("wfhanditem", "" + User.GetClient().GetHabbo().Username + ", acabas de recibir una bebida u otro objeto similar por un efecto de Wired.", ""));
+            Item.UpdateState(false, true);
+            Item.RequestUpdate(2, true);
+
+
+
+            return true;
+        }
+    }
+}
---
+++ HabboHotel/Items/Wired/WiredBoxType.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quasar.HabboHotel.Items.Wired
+{
+    public enum WiredBoxType
+    {
+        None,
+        TriggerRoomEnter,
+        TriggerUserSays,
+        TriggerRepeat,
+        TriggerLongRepeat,
+        TriggerStateChanges,
+        TriggerWalkOnFurni,
+        TriggerWalkOffFurni,
+        TriggerGameStarts,
+        TriggerGameEnds,
+        TriggerUserFurniCollision,
+        TriggerUserSaysCommand,
+        TriggerAtGivenTime,
+
+        EffectShowMessage,
+        EffectTeleportToFurni,
+        EffectToggleNegativeFurniState,
+        EffectToggleFurniState,
+        EffectKickUser,
+        EffectMatchPosition,
+        EffectMoveAndRotate,
+        EffectMoveFurniToNearestUser,
+        EffectMoveFurniFromNearestUser,
+        EffectMuteTriggerer,
+        EffectGiveReward,
+        EffectExecuteWiredStacks,
+        EffectAddScore,
+        EffectAddRewardPoints,
+        EffectApplyClothes,
+        EffectMoveUser,
+        EffectTimerReset,
+        EffectMoveToDir,
+        EffectProgressUserAchievement,
+
+        EffectTeleportBotToFurniBox,
+        EffectBotChangesClothesBox,
+        EffectBotMovesToFurniBox,
+        EffectBotCommunicatesToAllBox,
+        EffectBotCommunicatesToUserBox,
+        EffectBotFollowsUserBox,
+        EffectBotGivesHanditemBox,
+
+        EffectAddActorToTeam,
+        EffectRemoveActorFromTeam,
+        EffectSetRollerSpeed,
+        EffectRegenerateMaps,
+        EffectGiveUserBadge,
+        EffectGiveUserHanditem,
+        EffectGiveUserEnable,
+        EffectGiveUserDance,
+        EffectGiveUserFastwalk,
+        EffectGiveUserFreeze,
+
+        ConditionFurniHasUsers,
+        ConditionFurniHasFurni,
+        ConditionTriggererOnFurni,
+        ConditionIsGroupMember,
+        ConditionIsNotGroupMember,
+        ConditionTriggererNotOnFurni,
+        ConditionFurniHasNoUsers,
+        ConditionIsWearingBadge,
+        ConditionIsWearingFX,
+        ConditionIsNotWearingBadge,
+        ConditionIsNotWearingFX,
+        ConditionMatchStateAndPosition,
+        ConditionDontMatchStateAndPosition,
+        ConditionUserCountInRoom,
+        ConditionUserCountDoesntInRoom,
+        ConditionFurniTypeMatches,
+        ConditionFurniTypeDoesntMatch,
+        ConditionFurniHasNoFurni,
+        ConditionActorHasHandItemBox,
+        ConditionActorHasNotHandItemBox,
+        ConditionActorIsInTeamBox,
+        ConditionActorIsNotInTeamBox,
+        ConditionWearingClothes,
+        ConditionNotWearingClothes,
+        ConditionDateRangeActive,
+        ConditionLessSecs,
+        ConditionMoreSecs,
+        SendCustomMessageBox,
+        TotalUsersCoincidence,
+
+        AddonRandomEffect
+    }
+}
