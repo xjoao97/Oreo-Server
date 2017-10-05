5272c5b2de3de12476c0ce35ad0d5326bbd8a674
Lucas Hen <lhdsa@icloud.com>
2017-10-05 18:00:21 -0300

    Console commands
    Atalho para comandos no debug.

--- Core/ConsoleCommandHandler.cs
+++ Core/ConsoleCommandHandler.cs
@@ -6,6 +6,9 @@ using Quasar.Communication.Packets.Outgoing.Moderation;
 using System.Threading;
 using System.Threading.Tasks;
 using Quasar.Communication.Packets.Outgoing.Notifications;
+using Quasar.Communication.Packets.Outgoing.Catalog;
+using Quasar.Communication.Packets.Outgoing.Rooms.Notifications;
+using static Quasar.HabboHotel.GameClients.GameClient;
 
 namespace Quasar.Core
 {
@@ -26,25 +29,13 @@ namespace Quasar.Core
                 switch (parameters[0].ToLower())
                 {
                     #region stop
+                    case "stop":
+                    case "desligar":
                     case "shutdown":
                         {
-                            string time = parameters[1];
-                            string time2 = parameters[2];
-
-                            int total_time = int.Parse(time) * 60 * 1000;
-                            Logging.WriteLine("The server will be close in " + time + " minutes.", ConsoleColor.Yellow);
-                            QuasarEnvironment.GetGame().GetClientManager().SendMessage(new HotelWillCloseInMinutesAndBackInComposer(int.Parse(time), int.Parse(time2)));
-                            QuasarStaticGameSettings.IsGoingToBeClose = true;
-                            Task t = Task.Factory.StartNew(() => ShutdownIn(total_time));
-                            break;
-                        }
-                    #endregion
-
-                    #region stop
-                    case "open":
-                        {
-                            QuasarStaticGameSettings.HotelOpenForUsers = true;
-                            Logging.WriteLine("Now users can enter.", ConsoleColor.Yellow);
+                            Logging.DisablePrimaryWriting(true);
+                            Logging.WriteLine("The server is saving users furniture, rooms, etc. WAIT FOR THE SERVER TO CLOSE, DO NOT EXIT THE PROCESS IN TASK MANAGER!!", ConsoleColor.Yellow);
+                            QuasarEnvironment.PerformShutDown();
                             break;
                         }
                     #endregion
@@ -53,17 +44,76 @@ namespace Quasar.Core
                     case "alert":
                         {
                             string Notice = inputData.Substring(6);
+                            //QuasarEnvironment.GetGame().GetClientManager().SendMessage(new RoomNotificationComposer("Mensagem dos Desenvolvedores", ""+ Notice + "", "frank_notify", ""));
 
-                            QuasarEnvironment.GetGame().GetClientManager().SendMessage(new BroadcastMessageAlertComposer(QuasarEnvironment.GetGame().GetLanguageLocale().TryGetValue("console.noticefromadmin") + "\n\n" + Notice));
+                            QuasarEnvironment.GetGame().GetClientManager().SendMessage(new BroadcastMessageAlertComposer(Notice));
+                            Console.WriteLine("Alerta enviada com sucesso!", ConsoleColor.DarkGreen);
+                            break;
+                        }
+                    #endregion
 
-                            log.Info(">> [SEND] Alerta enviada satisfactoriamente");
+                    #region help
+                    case "help":
+                    case "ajda":
+                    case "sirio":
+                    case "h":
+                        {
+                            Console.WriteLine("\n\nComandos disponíveis pelo Hen:\n\nnotification/info/alert - Alertar todos do hotel.\n\nstop - Desligar o emulador por completo.\n\nclear - Limpar essa tela do emulador.\n\ncata - Atualizar a Loja.\n\nitems - Atualizar items da Loja.\n\nnav - Atualize o navegador.\n\npolls - Atualize as Pesquisas.");
+                            break;
+                        }
+                    #endregion
+
+                    #region cata
+                    case "cata":
+                    case "catalog":
+                    case "shop":
+                    case "c":
+                        {
+                            QuasarEnvironment.GetGame().GetCatalog().Init(QuasarEnvironment.GetGame().GetItemManager());
+                            QuasarEnvironment.GetGame().GetLandingManager().LoadPromotions();
+                            QuasarEnvironment.GetGame().GetClientManager().SendMessage(new CatalogUpdatedComposer());
+                            Console.WriteLine("Loja atualizada.", ConsoleColor.DarkGreen);
+                            break;
+                        }
+                    #endregion
+
+                    #region items
+                    case "items":
+                    case "i":
+                        {
+                            QuasarEnvironment.GetGame().GetItemManager().Init();
+                            Console.WriteLine("Items atualizados.", ConsoleColor.DarkGreen);
+                            break;
+                        }
+                    #endregion
+
+                    #region clear
+                    case "clear":
+                        {
+                            Console.Clear();
+                            Console.WriteLine();
+                            Console.WriteLine(@" Para mais informações visite - sirio.wulles.url.ph.", ConsoleColor.DarkCyan);
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine(@"--------------------------------------------------------------------------------");
+                            break;
+                        }
+                    #endregion
+
+                    #region navigator
+                    case "navigator":
+                    case "nav":
+                    case "n":
+                        {
+                            QuasarEnvironment.GetGame().GetNavigator().Init();
+                            Console.WriteLine("Navegadr atualizado.", ConsoleColor.DarkGreen);
                             break;
                         }
                     #endregion
 
                     default:
                         {
-                            log.Error(parameters[0].ToLower() + "? No se ha conseguido ese comando, escribe Help para mas Informacion.");
+                            Console.WriteLine(parameters[0].ToLower() + " é um comando invalido no Sirio.", ConsoleColor.DarkRed);
                             break;
                         }
                 }
