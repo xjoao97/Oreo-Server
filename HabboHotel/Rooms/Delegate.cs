e9b05b13cda2943572a54a964ece12e6e1aa5eee
joao pedro <joaopedro12_psy@hotmail.com>
2018-02-11 17:29:52 +0000

    Deleted Not Fixed

--- HabboHotel/Rooms/Delegate.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Oreo.HabboHotel.Rooms
-{
-    public delegate void RoomEventDelegate(object sender, EventArgs e);
-    public delegate void UserWalksFurniDelegate(object sender, UserWalksOnArgs e);
-}
