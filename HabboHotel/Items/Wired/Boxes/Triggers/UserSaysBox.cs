96f1bd37381930ee6bf542682de629615472ad1f
joao pedro <joaopedro12_psy@hotmail.com>
2018-03-08 14:22:35 +0000

    Updated UserSaysBox fixed e Security #fix148

--- HabboHotel/Items/Wired/Boxes/Triggers/UserSaysBox.cs
+++ HabboHotel/Items/Wired/Boxes/Triggers/UserSaysBox.cs
@@ -1,27 +1,28 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Text;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 
-using Quasar.Communication.Packets.Incoming;
-using Quasar.HabboHotel.Rooms;
-using Quasar.HabboHotel.Users;
-using Quasar.Communication.Packets.Outgoing.Rooms.Chat;
+using Emulator.HabboHotel.Rooms;
+using Emulator.HabboHotel.Users;
+using Emulator.Communication.Packets.Incoming;
+using Emulator.Communication.Packets.Outgoing.Rooms.Chat;
+using Emulator.HabboHotel.Rooms.Chat.Commands;
 
-namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
+namespace Emulator.HabboHotel.Items.Wired.Boxes.Triggers
 {
-    class UserSaysBox : IWiredItem
+    class UserSaysCommandBox : IWiredItem
     {
         public Room Instance { get; set; }
         public Item Item { get; set; }
-        public WiredBoxType Type { get { return WiredBoxType.TriggerUserSays; } }
+        public WiredBoxType Type { get { return WiredBoxType.TriggerUserSaysCommand; } }
         public ConcurrentDictionary<int, Item> SetItems { get; set; }
         public string StringData { get; set; }
         public bool BoolData { get; set; }
         public string ItemsData { get; set; }
 
-        public UserSaysBox(Room Instance, Item Item)
+        public UserSaysCommandBox(Room Instance, Item Item)
         {
             this.Instance = Instance;
             this.Item = Item;
@@ -36,8 +37,7 @@ namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
             string Message = Packet.PopString();
 
             this.BoolData = OwnerOnly == 1;
-            this.StringData = Message.ToLower();
-
+            this.StringData = Message;
         }
 
         public bool Execute(params object[] Params)
@@ -50,12 +50,13 @@ namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
             if (User == null)
                 return false;
 
-            string Message = Convert.ToString(Params[1]);
-
-            if ((BoolData && Instance.OwnerId != Player.Id) || Player == null || string.IsNullOrWhiteSpace(Message) || string.IsNullOrWhiteSpace(this.StringData))
+            if ((BoolData && Instance.OwnerId != Player.Id) || string.IsNullOrWhiteSpace(this.StringData))
                 return false;
 
-            if (Message.ToLower().Contains(" " + this.StringData.ToLower()) || Message.ToLower().Contains(" " + this.StringData.ToLower() + " ") || Message.ToLower().Contains(this.StringData.ToLower() + " ") || Message.ToLower().Contains(this.StringData.ToLower()))
+            if (!HabboEnvironment.GetGame().GetChatManager().GetCommands().TryGetCommand(this.StringData.Replace(":", "").ToLower(), out IChatCommand ChatCommand))
+                return false;
+
+            if (Player.IChatCommand == ChatCommand)
             {
                 Player.WiredInteraction = true;
                 ICollection<IWiredItem> Effects = Instance.GetWired().GetEffects(this);
@@ -69,18 +70,22 @@ namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
                     Instance.GetWired().OnEvent(Condition.Item);
                 }
 
-                Player.GetClient().SendMessage(new WhisperComposer(User.VirtualId, Message, 0, 0));
-                bool HasRandomEffectAddon = Effects.Where(x => x.Type == WiredBoxType.AddonRandomEffect).ToList().Count() > 0;
+                Player.GetClient().SendMessage(new WhisperComposer(User.VirtualId, this.StringData, 0, 0));
+                //Check the ICollection to find the random addon effect.
+                bool HasRandomEffectAddon = Effects.Count(x => x.Type == WiredBoxType.AddonRandomEffect) > 0;
                 if (HasRandomEffectAddon)
                 {
+                    //Okay, so we have a random addon effect, now lets get the IWiredItem and attempt to execute it.
                     IWiredItem RandomBox = Effects.FirstOrDefault(x => x.Type == WiredBoxType.AddonRandomEffect);
                     if (!RandomBox.Execute())
                         return false;
 
+                    //Success! Let's get our selected box and continue.
                     IWiredItem SelectedBox = Instance.GetWired().GetRandomEffect(Effects.ToList());
                     if (!SelectedBox.Execute())
                         return false;
 
+                    //Woo! Almost there captain, now lets broadcast the update to the room instance.
                     if (Instance != null)
                     {
                         Instance.GetWired().OnEvent(RandomBox.Item);
@@ -97,7 +102,6 @@ namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
                         Instance.GetWired().OnEvent(Effect.Item);
                     }
                 }
-
                 return true;
             }
 
@@ -105,3 +109,4 @@ namespace Quasar.HabboHotel.Items.Wired.Boxes.Triggers
         }
     }
 }
+
