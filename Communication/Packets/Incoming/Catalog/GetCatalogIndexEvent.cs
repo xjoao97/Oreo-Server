c36595c6ccb754516b765aff1d4e3cae8e5e7101
joao pedro <joaopedro12_psy@hotmail.com>
2018-03-17 18:02:30 +0000

    CatalogIndex clean up and sub pages #fix 2 new <<<<

--- Communication/Packets/Incoming/Catalog/GetCatalogIndexEvent.cs
+++ Communication/Packets/Incoming/Catalog/GetCatalogIndexEvent.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using Quasar.Communication.Packets.Incoming;
+using Oreo.HabboHotel.GameClients;
+using Oreo.Communication.Packets.Outgoing.Catalog;
+using Oreo.Communication.Packets.Outgoing.BuildersClub;
 
-using Quasar.HabboHotel.GameClients;
-using Quasar.Communication.Packets.Outgoing.Catalog;
-using Quasar.Communication.Packets.Outgoing.BuildersClub;
-
-namespace Quasar.Communication.Packets.Incoming.Catalog
+namespace Oreo.Communication.Packets.Incoming.Catalog
 {
     public class GetCatalogIndexEvent : IPacketEvent
     {
         public void Parse(GameClient Session, ClientPacket Packet)
         {
 
-            Session.SendMessage(new CatalogIndexComposer(Session, QuasarEnvironment.GetGame().GetCatalog().GetPages(), "NORMAL"));
-            Session.SendMessage(new CatalogIndexComposer(Session, QuasarEnvironment.GetGame().GetCatalog().GetBCPages(), "BUILDERS_CLUB"));
-
+            Session.SendMessage(new CatalogIndexComposer(Session, OreoServer.GetGame().GetCatalog().GetPages(Session, -1)));
             Session.SendMessage(new CatalogItemDiscountComposer());
             Session.SendMessage(new BCBorrowedItemsComposer());
         }
