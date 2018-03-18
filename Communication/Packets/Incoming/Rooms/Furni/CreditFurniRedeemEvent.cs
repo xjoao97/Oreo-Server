278fc570259cd65c456c4829d878b1b2a9beec4f
joao pedro <joaopedro12_psy@hotmail.com>
2018-03-18 23:31:57 +0000

    Código Refeito CreditFurniRedeemEvent Fixado compra de 2 mobs #fix170

--- Communication/Packets/Incoming/Rooms/Furni/CreditFurniRedeemEvent.cs
+++ Communication/Packets/Incoming/Rooms/Furni/CreditFurniRedeemEvent.cs
@@ -1,77 +1,107 @@
-using Oreo.HabboHotel.Rooms;
-using Oreo.HabboHotel.Items;
-using Oreo.Communication.Packets.Outgoing.Inventory.Purse;
-using Oreo.Communication.Packets.Outgoing.Inventory.Furni;
-using Oreo.Database.Interfaces;
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
 
-namespace Oreo.Communication.Packets.Incoming.Rooms.Furni
+using Emulator.HabboHotel.Rooms;
+using Emulator.HabboHotel.Items;
+using Emulator.Communication.Packets.Outgoing.Inventory.Purse;
+using Emulator.Communication.Packets.Outgoing.Inventory.Furni;
+
+using Emulator.Database.Interfaces;
+
+
+namespace Emulator.Communication.Packets.Incoming.Rooms.Furni
 {
-	class CreditFurniRedeemEvent : IPacketEvent
-	{
-		public void Parse(HabboHotel.GameClients.GameClient Session, ClientPacket Packet)
-		{
-			if (!Session.GetHabbo().InRoom)
-				return;
+    class CreditFurniRedeemEvent : IPacketEvent
+    {
+        public void Parse(HabboHotel.GameClients.GameClient Session, ClientPacket Packet)
+        {
+            if (!Session.GetHabbo().InRoom)
+                return;
+
+            if (!HabboEnvironment.GetGame().GetRoomManager().TryGetRoom(Session.GetHabbo().CurrentRoomId, out Room Room))
+                return;
+
+            if (!Room.CheckRights(Session, true))
+                return;
+
+            if (HabboEnvironment.GetDBConfig().DBData["exchange_enabled"] != "1")
+            {
+                Session.SendNotification("The hotel managers have temporarilly disabled exchanging!");
+                return;
+            }
+
+            Item Exchange = Room.GetRoomItemHandler().GetItem(Packet.PopInt());
+            if (Exchange == null)
+                return;
+
+            if (Exchange.Data.InteractionType != InteractionType.EXCHANGE)
+                return;
 
 
-			if (!OreoServer.GetGame().GetRoomManager().TryGetRoom(Session.GetHabbo().CurrentRoomId, out Room Room))
-				return;
+            int Value = Exchange.Data.BehaviourData;
 
-			if (!Room.CheckRights(Session, true))
-				return;
+            if (Exchange.Data.ItemName.StartsWith("POINTS_"))
+            {
+               
+                    using (IQueryAdapter dbClient = HabboEnvironment.GetDatabaseManager().GetQueryReactor())
+                    {
+                        dbClient.SetQuery("DELETE FROM `items` WHERE `id` = @exchangeId LIMIT 1");
+                        dbClient.AddParameter("exchangeId", Exchange.Id);
+                        dbClient.RunQuery();
+                    }
+                    Session.SendWhisper("You have successfully redeemed " + Value + " point(s)!");
+                
 
-			if (OreoServer.GetGame().GetSettingsManager().TryGetValue("room.item.exchangeables.enabled") != "1")
-			{
-				Session.SendNotification("Desculpe, mas as moedas foram  desativadas !");
-				return;
-			}
+                if (Value > 0)
+                {
+                    Session.GetHabbo().GOTWPoints = Session.GetHabbo().GOTWPoints + Value;
+                    Session.SendMessage(new HabboActivityPointNotificationComposer(Session.GetHabbo().GOTWPoints, Value, 103));
+                }
 
-			Item Exchange = Room.GetRoomItemHandler().GetItem(Packet.PopInt());
-			if (Exchange == null)
-				return;
+                Session.SendMessage(new FurniListUpdateComposer());
+                Room.GetRoomItemHandler().RemoveFurniture(null, Exchange.Id, false);
+                Session.GetHabbo().GetInventoryComponent().RemoveItem(Exchange.Id);     
+            }
+            else if (Exchange.Data.ItemName.StartsWith("DF_"))
+            {
+                if (Value > 0)
+                {
+                    Session.GetHabbo().Diamonds += Value;
+                    Session.SendMessage(new ActivityPointsComposer(Session.GetHabbo().Diamonds, Session.GetHabbo().Diamonds, 5));
+                }
 
-			if (!Exchange.GetBaseItem().ItemName.StartsWith("CF_") && !Exchange.GetBaseItem().ItemName.StartsWith("CFC_") && !Exchange.GetBaseItem().ItemName.StartsWith("DF_") && !Exchange.GetBaseItem().ItemName.StartsWith("DFD_"))
-				return;
+                using (IQueryAdapter dbClient = HabboEnvironment.GetDatabaseManager().GetQueryReactor())
+                {
+                    dbClient.SetQuery("DELETE FROM `items` WHERE `id` = @exchangeId LIMIT 1");
+                    dbClient.AddParameter("exchangeId", Exchange.Id);
+                    dbClient.RunQuery();
+                }
 
-			string[] Split = Exchange.GetBaseItem().ItemName.Split('_');
-			int Valuee = int.Parse(Split[1]);
+                Session.SendMessage(new FurniListUpdateComposer());
+                Room.GetRoomItemHandler().RemoveFurniture(null, Exchange.Id, false);
+                Session.GetHabbo().GetInventoryComponent().RemoveItem(Exchange.Id);
+            }
+            else
+            {
+                if (Value > 0)
+                {
+                    Session.GetHabbo().Credits += Value;
+                    Session.SendMessage(new CreditBalanceComposer(Session.GetHabbo().Credits));
+                }
 
-			if (Valuee > 0)
-			{
-				if (Exchange.GetBaseItem().ItemName.StartsWith("CF_") || Exchange.GetBaseItem().ItemName.StartsWith("CFC_"))
-				{
-					Session.GetHabbo().Credits += Valuee;
-					Session.SendMessage(new CreditBalanceComposer(Session.GetHabbo().Credits));
-				}
-				else if (Exchange.GetBaseItem().ItemName.StartsWith("DF_") || Exchange.GetBaseItem().ItemName.StartsWith("DFD_"))
-				{
-					Session.GetHabbo().Diamonds += Valuee;
-					Session.SendMessage(new HabboActivityPointNotificationComposer(Session.GetHabbo().Diamonds, Valuee, 5));
-				}
-			}
+                using (IQueryAdapter dbClient = HabboEnvironment.GetDatabaseManager().GetQueryReactor())
+                {
+                    dbClient.SetQuery("DELETE FROM `items` WHERE `id` = @exchangeId LIMIT 1");
+                    dbClient.AddParameter("exchangeId", Exchange.Id);
+                    dbClient.RunQuery();
+                }
 
-			if (Exchange.Data.InteractionType != InteractionType.EXCHANGE)
-				return;
-
-			int Value = Exchange.Data.BehaviourData;
-
-			if (Value > 0)
-			{
-				Session.GetHabbo().Credits += Value;
-				Session.SendMessage(new CreditBalanceComposer(Session.GetHabbo().Credits));
-			}
-
-			using (IQueryAdapter dbClient = OreoServer.GetDatabaseManager().GetQueryReactor())
-			{
-				dbClient.SetQuery("DELETE FROM `items` WHERE `id` = @exchangeId LIMIT 1");
-				dbClient.AddParameter("exchangeId", Exchange.Id);
-				dbClient.RunQuery();
-			}
-
-			Session.SendMessage(new FurniListUpdateComposer());
-			Room.GetRoomItemHandler().RemoveFurniture(null, Exchange.Id, false);
-			Session.GetHabbo().GetInventoryComponent().RemoveItem(Exchange.Id);
-
-		}
-	}
+                Session.SendMessage(new FurniListUpdateComposer());
+                Room.GetRoomItemHandler().RemoveFurniture(null, Exchange.Id, false);
+                Session.GetHabbo().GetInventoryComponent().RemoveItem(Exchange.Id);
+            }
+        }
+    }
 }
