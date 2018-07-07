789f01e1cb7b06232e0b67d9758b2b2808cbda2b
joao pedro <joaopedro12_psy@hotmail.com>
2018-07-07 22:06:26 +0000

    Fixado FurniListNotificationComposer.cs (Compra de Mobs mais rápida)

--- Communication/Packets/Outgoing/Inventory/Furni/FurniListNotificationComposer.cs
+++ Communication/Packets/Outgoing/Inventory/Furni/FurniListNotificationComposer.cs
@@ -1,13 +1,26 @@
-﻿using System;
+using Plus.HabboHotel.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Quasar.Communication.Packets.Outgoing.Inventory.Furni
+ 
+namespace Plus.Communication.Packets.Outgoing.Inventory.Furni
 {
     class FurniListNotificationComposer : ServerPacket
     {
+        public FurniListNotificationComposer(List<Item> items, int Type)
+            : base(ServerPacketHeader.FurniListNotificationMessageComposer)
+        {
+            base.WriteInteger(1);
+            base.WriteInteger(Type);
+            base.WriteInteger(items.Count);
+            foreach(Item i in items)
+            {
+                base.WriteInteger(i.Id);
+            }
+        }
+ 
         public FurniListNotificationComposer(int Id, int Type)
             : base(ServerPacketHeader.FurniListNotificationMessageComposer)
         {
@@ -16,5 +29,6 @@ namespace Quasar.Communication.Packets.Outgoing.Inventory.Furni
             base.WriteInteger(1);
             base.WriteInteger(Id);
         }
+ 
     }
 }
